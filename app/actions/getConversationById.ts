af638859da33ac27ca9d1d131272efb806b5d8bf
Matheus de Arruda <matheusarrudaf@gmail.com>
2024-01-27 22:22:20 -0300

    actions att

--- app/actions/getConversationById.ts
+++ app/actions/getConversationById.ts
@@ -1,33 +1,30 @@
-import prisma from '@/app/libs/prismadb';
-import getCurrentUser from './getCurrentUser';
-import ConversationId from '../conversations/[conversationId]/page';
-
+import prisma from "@/app/libs/prismadb";
+import getCurrentUser from "./getCurrentUser";
 
 const getConversationById = async (
-    conversationId: string
+  conversationId: string
 ) => {
-    try {
+  try {
+    const currentUser = await getCurrentUser();
 
-        const currentUser = await getCurrentUser();
-
-        if(!currentUser?.email){
-            return null;
-        }
-
-        const conversation = await prisma.conversation.findUnique({
-            where: {
-                id: conversationId
-            },
-            include: {
-                users: true
-            }
-        });
-
-        return conversation;
-
-    } catch(error: any){
-        return null;
+    if (!currentUser?.email) {
+      return null;
     }
+  
+    const conversation = await prisma.conversation.findUnique({
+      where: {
+        id: conversationId
+      },
+      include: {
+        users: true,
+      },
+    });
+
+    return conversation;
+  } catch (error: any) {
+    console.log(error, 'SERVER_ERROR')
+    return null;
+  }
 };
 
 export default getConversationById;
--- app/actions/getMessages.ts
+++ app/actions/getMessages.ts
@@ -1,28 +1,26 @@
-import prisma from '@/app/libs/prismadb';
-import ConversationId from '../conversations/[conversationId]/page';
+import prisma from "@/app/libs/prismadb";
 
 const getMessages = async (
-    ConversationId: string
+  conversationId: string
 ) => {
-    try {
-        const messages = await prisma.message.findMany({
-            where: {
-                conversationId: ConversationId
-            },
-            include: {
-                sender: true,
-                seen: true
-            },
-            orderBy: {
-                createdAt: 'asc'
-            }
-        });
+  try {
+    const messages = await prisma.message.findMany({
+      where: {
+        conversationId: conversationId
+      },
+      include: {
+        sender: true,
+        seen: true,
+      },
+      orderBy: {
+        createdAt: 'asc'
+      }
+    });
 
-        return messages;
-
-    } catch (error: any){
-        return [];
-    }
+    return messages;
+  } catch (error: any) {
+    return [];
+  }
 };
 
 export default getMessages;
